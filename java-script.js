472daa0b8cb3d799a8ef36bbf6bb542e729c27c0
saadbavo <[saadhmk13@gmail.com]>
2024-10-25 12:28:13 +0100

    Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers

--- java-script.js
+++ java-script.js
@@ -1,3 +1,4 @@
+
 function add(a, b){
     return a + b ;
 
@@ -11,4 +12,11 @@ function multiply(arr){
 };
 function divide(a, b){
     return a/b; 
+};
+
+let num1 = x;
+let num2 = y;
+let operator = "+";
+function operate(num1, num2, operator){
+return add(num1, num2)
 };
