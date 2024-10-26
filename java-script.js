d396681983439e0c054f2992fb5e08039079c2d1
saadbavo <[saadhmk13@gmail.com]>
2024-10-26 12:24:50 +0100

    Create the functions that populate the display when you click the number buttons Linke all file whit html  fille

--- java-script.js
+++ java-script.js
@@ -14,10 +14,22 @@ function divide(a, b){
     return a/b; 
 };
 
-let num1 = x;
-let num2 = y;
+let num1 = 0;
+let num2 = 0;
 let operator = " ";
 function operate(num1, num2, operator){
     if (operator === "+"){
    return add(num1, num2)};
 };
+let displayValue= 0;
+let displayArea = document.getElementById('result');
+const numbutton = document.querySelectorAll(".num");
+numbutton.forEach(nuM =>{
+    addEventListener('click' , (e) =>{
+        updateDisplay(e)
+    })
+});
+let updateDisplay = (e) =>{
+    displayValue = e.target.id;
+    displayArea.innerHTML = displayValue;
+}
